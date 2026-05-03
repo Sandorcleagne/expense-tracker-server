9ff0cef73ef6ebb14961cf9e1aba04e8d60e8d56
Harsh <harshjoshi2k@gmail.com>
2026-05-03 20:51:14 +0530

    fix 401 issue

--- constant.ts
+++ constant.ts
@@ -5,6 +5,6 @@ export const DB_Name = "expense-tracker";
 export const options: CookieOptions = {
   httpOnly: config?.env === "production" ? true : false,
   secure: config?.env === "production",
-  sameSite: "lax",
+  sameSite: config?.env === "production" ? "none" : "lax",
   path: "/",
 };
